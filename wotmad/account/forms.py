96f131995e0ee66945712ca30c486a1c9e91ef28
Alex Vidal <alex.vidal@gmail.com>
2012-03-23 23:08:31 -0500

    More validations on username.

--- wotmad/account/forms.py
+++ wotmad/account/forms.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 
 
 class AccountSetupForm(forms.Form):
-    username = forms.CharField()
+    username = forms.RegexField(label="Username", max_length=30,
+        regex=r'^[\w.@+-]+$',
+        error_messages={
+            'invalid': ("This value may contain only letters, numbers and "
+                        "@/./+/-/_ characters.")})
 
     def __init__(self, user, *args, **kwargs):
         super(AccountSetupForm, self).__init__(*args, **kwargs)
