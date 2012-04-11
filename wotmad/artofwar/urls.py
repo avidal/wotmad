0e0fb59d1b911fc5a416b496fa11e505c14df64b
Alex Vidal <alex.vidal@gmail.com>
2012-04-10 21:54:35 -0500

    Fix artofwar urls.

--- wotmad/artofwar/urls.py
+++ wotmad/artofwar/urls.py
@@ -5,8 +5,8 @@ from .views import LogList, SubmitLog, LogDetail
 urlpatterns = patterns(
     'wotmad.artofwar.views',
 
-    url(r'art-of-war/$', LogList.as_view(), name='list'),
-    url(r'art-of-war/submit/$', SubmitLog.as_view(), name='submit'),
-    url(r'art-of-war/(?P<pk>\d+)/(?P<slug>[a-z0-9\-_]+)/', LogDetail.as_view(),
+    url(r'^$', LogList.as_view(), name='list'),
+    url(r'^submit/$', SubmitLog.as_view(), name='submit'),
+    url(r'^(?P<pk>\d+)/(?P<slug>[a-z0-9\-_]+)/', LogDetail.as_view(),
         name='detail'),
 )
