eb97f15fb2f48e329060895064eae5377dbfae6e
Alex Vidal <alex.vidal@gmail.com>
2012-04-11 16:29:33 -0500

    Add contribute to stats page.

--- wotmad/stats/urls.py
+++ wotmad/stats/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import StatList
+from .views import StatList, ContributeStat
 
 urlpatterns = patterns(
     'wotmad.stats.views',
 
     url(r'^$', StatList.as_view(), name='list'),
+    url(r'^contribute/$', ContributeStat.as_view(), name='contribute'),
 )
--- wotmad/stats/views.py
+++ wotmad/stats/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView
 
 from .models import Stat
 
@@ -8,3 +8,7 @@ class StatList(ListView):
 
     def get_queryset(self):
         return Stat.objects.order_by('-date_submitted')
+
+
+class ContributeStat(TemplateView):
+    template_name = 'stats/contribute.html'
