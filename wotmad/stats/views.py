2ae9074b3af64ae94f4f6dd650c82334827262ba
Alex Vidal <alex.vidal@gmail.com>
2012-05-31 09:13:24 -0500

    Limit stat listing to the last 100 submissions.

--- wotmad/stats/views.py
+++ wotmad/stats/views.py
@@ -11,7 +11,7 @@ class StatList(ListView):
     model = Stat
 
     def get_queryset(self):
-        return Stat.objects.order_by('-date_submitted')
+        return Stat.objects.order_by('-date_submitted')[:100]
 
 
 class ContributeStat(TemplateView):
